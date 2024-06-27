d1bd219f63ff6ee1cdf9ff05ab252b1b686b422a
aleira <aleira.redfury@gmail.com>
2024-06-27 15:54:21 +0200

    pipe di products

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,20 +3,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../interfaces/product';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, RouterLink],
+  imports: [MatCardModule, MatButtonModule, RouterLink, CommonModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  @Input () product!: Product 
-  @Output() myEvent = new EventEmitter<string>;
+  @Input() product!: Product;
+  @Output() myEvent = new EventEmitter<string>();
 
-
-emitEvent() {
-  this.myEvent.emit(this.product.name);
-}
+  emitEvent() {
+    this.myEvent.emit(this.product.name);
+  }
 }
