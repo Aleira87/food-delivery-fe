cd2cfce67d4cbd922321d0277abf19d61d2003e9
aleira <aleira.redfury@gmail.com>
2024-07-11 14:58:36 +0200

    Modifiche grafiche

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { CartItem } from './../../interfaces/cart-item';
 import { CartService } from './../../services/cart.service';
-import { Component, effect, OnInit } from '@angular/core';
+import { Component, effect, OnInit, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent implements OnInit {
   cartItems: CartItem[] = [];
   cartCount: number = 0;
+
   constructor(private cartService: CartService) {
     effect(() => {
       this.cartCount = this.cartService.getCartItems().length;
